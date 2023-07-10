9a7d03941cd06e043ea3784946473a014d6a9a79
DavidALeo <128328646+DavidALeo@users.noreply.github.com>
2023-07-11 00:00:43 +0200

    Added user manual reference

--- R/mod_about.R
+++ R/mod_about.R
@@ -14,6 +14,7 @@ mod_about_ui <- function(id){
     p("Este es un proyecto de an\u00e1lisis de datos utilizando el marco de trabajo Shiny de R.
       El objetivo principal de este proyecto es proporcionar una herramienta interactiva
       para explorar, visualizar y analizar datos relacionados con el ", a(href="https://www.boe.es/buscar/doc.php?id=BOE-A-2008-17629", "Real Decreto 1801/2008"),"."),
+    p("Puedes consultar también el ", a(href="www/user_manual.pdf", "manual de usuario"),"."),
     p("Para m\u00e1s detalles sobre el c\u00f3digo fuente y la implementaci\u00f3n, por favor visite nuestro ",
       a(href="https://github.com/DavidALeo/PlacidoProject", "repositorio de GitHub"), "."),
     h3("Autores"),
